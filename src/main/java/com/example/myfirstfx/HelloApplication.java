14ea91307d1c40e7181513eeeeff1db8e3fdfd7c
IIvanov57 <ivanov080892@mail.ru>
2023-09-14 22:43:24 +0300

    Initial commit

---
+++ src/main/java/com/example/myfirstfx/HelloApplication.java
@@ -0,0 +1,25 @@
+package com.example.myfirstfx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 635, 463);
+        stage.setTitle("Calculator");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
