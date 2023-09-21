f3c23b80d92cf9467d98a7db1599a90a52897f4a
IIvanov57 <ivanov080892@mail.ru>
2023-09-21 22:41:28 +0300

    add all metods

--- src/main/java/com/example/myfirstfx/Controller.java
+++ src/main/java/com/example/myfirstfx/Controller.java
@@ -4,40 +4,40 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
+import java.util.Arrays;
+
 public class Controller {
 
     @FXML
-    private Button degreeButton;
+    private Button degreeButton; //кнопка возведения в степень x^y
 
     @FXML
-    private Button increaseButton;
+    private Button increaseButton; //кнопка умножения
 
     @FXML
-    private TextField inputNumberOne;
+    private TextField inputNumberOne;//поле ввода первого числа
 
     @FXML
-    private TextField inputNumberTwo;
+    private TextField inputNumberTwo;//поле ввода второго числа
 
     @FXML
-    private Button minusButton;
+    private Button minusButton;//кнопка операции вычитания
 
     @FXML
-    private Button plusButton;
+    private Button plusButton;//копка операции сложения
+    @FXML
+    private Button resetButton;//кнопка сброса полей ввода
 
     @FXML
-    private Button resetButton;
+    private Button splittingButton;//кнопка деления
 
     @FXML
-    private Button splittingButton;
-
-    @FXML
-    private TextField outputResult;
+    private TextField outputResult;//поле вывода результа операции
 
     @FXML
     void actionMinusButton() {
         System.out.println("interaction minus button");
-        double result = getNumberOne() - getNumberTwo();
-        outputResult.setText(String.valueOf(result));
+        outputResult.setText(String.valueOf(Math.abs(getNumberOne() - getNumberTwo())));
     }
 
     double getNumberOne() {
@@ -45,7 +45,12 @@ public class Controller {
             return 0;
         }
         System.out.println("number1 " + inputNumberOne.getText());
-        return Double.parseDouble(inputNumberOne.getText());
+        try {
+            return Double.parseDouble(inputNumberOne.getText());
+        } catch (Exception e) {
+            System.out.println(Arrays.toString(e.getStackTrace()));
+        }
+        return 0;
     }
 
     double getNumberTwo() {
@@ -53,31 +58,43 @@ public class Controller {
             return 0;
         }
         System.out.println("number1 " + inputNumberTwo.getText());
-        return Double.parseDouble(inputNumberTwo.getText());
+        try {
+            return Double.parseDouble(inputNumberTwo.getText());
+        } catch (Exception e) {
+            System.out.println(Arrays.toString(e.getStackTrace()));
+        }
+        return 0;
     }
 
     @FXML
     void actionPlusButton() {
         System.out.println("interaction Plus");
+        outputResult.setText(String.valueOf(Math.abs(getNumberOne() + getNumberTwo())));
     }
 
     @FXML
     void actionDegreeButton() {
         System.out.println("int dgree");
+        outputResult.setText(String.valueOf(Math.pow(getNumberOne(),getNumberTwo())));
     }
 
     @FXML
     void actionIncreaseButton() {
         System.out.println("increaseButton");
+        outputResult.setText(String.valueOf(getNumberOne() * getNumberTwo()));
     }
 
     @FXML
     void actionResetButton() {
         System.out.println("resetButton");
+        inputNumberOne.setText("");
+        inputNumberTwo.setText("");
+        outputResult.setText("");
     }
 
     @FXML
     void actionSplittingButton() {
         System.out.println("splittingButton");
+        outputResult.setText(String.valueOf(getNumberOne() / getNumberTwo()));
     }
 }
